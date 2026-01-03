2c45aa7e6bf72664e1838c51ba1c1ea4978e8bb7
yViky <manateeviky@gmail.com>
2026-01-02 22:37:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'https://api.open-meteo.com/v1/forecast?latitude=-9.9747&longitude=-67.81&hourly=temperature_2m', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
