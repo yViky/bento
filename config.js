607fbd85edc1bc78f73a4401531c2d24af1b5130
yViky <manateeviky@gmail.com>
2026-01-02 22:14:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -11,16 +11,16 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Fei',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingMorning: 'Bom dia!',
+	greetingAfternoon: 'Boa tarde,',
+	greetingEvening: 'Boa noite,',
+	greetingNight: 'VAI DORMIR!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
